1f953e0c93ac781718d6d988bc78a6e5feaa9c18
Leo Markley <stencylxd@gmail.com>
2020-01-14 17:45:06 -0500

    wait what

--- rainbow_gem.rb
+++
@@ -1,7 +0,0 @@
-
-# rainbow_gem.rb | stencylxd 2019
-
-require "rainbow"; require "rainbow/refinement"; using Rainbow
-puts "Hello World!".red.bright; print "H".red.bright.underline
-print "e".blue.bright.underline; print "l".green.bright.underline
-print "l".magenta.bright.underline; print "o".yellow.bright.underline
