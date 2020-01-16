37c6eecab1dfda4e1a046f74740937c9dda33b14
Leo Markley <stencylxd@gmail.com>
2020-01-16 18:03:22 -0500

    Compacted and updated some programs

--- telent_random_number.rb
+++ telent_random_number.rb
@@ -2,10 +2,9 @@
 # telnet_random_number || stencylxd 2020
 
 # To run >>
-# Run 'telnet localhost 8080' in a unix terminal with telnet.
+# Run 'telnet localhost 8080' in a terminal with telnet.
 # Boom. Random number.
 
 require 'socket'; socket = TCPServer.new('localhost', 8080)
-puts "Generating on port 8080. Press CTRL+C to cancel."; loop do; client = socket.accept; Thread.new {
-client.write("\nTelnet number generator || stencylxd 2020\n\n")
-client.write("Your number is: " + rand(0..1000000).to_s + "\n\n"); client.close; }; end
+puts "Generating on port 8080. Press CTRL+C to cancel."; loop do; client = socket.accept
+Thread.new { client.write("Your number is: " + rand(0..1000000).to_s + "\n\n"); client.close; }; end
