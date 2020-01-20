3d6e30fbfa577fc5ad7a11433dfc135266fb1fda
Leo Markley <stencylxd@gmail.com>
2020-01-20 14:12:51 -0800

    Added cool method array thing

--- array-tools.rb
+++
@@ -1,9 +0,0 @@
-# array-tools || stencylxd 2020
-
-public;def mvo(x);l=self.length-1;if x>l # Method move one.
-raise"mvo: given pram is more than array length.";abort
-elsif l<0;raise"mvo: given array has no objects.";abort
-elsif x<0;raise"mvo: please specify a number higher than -1."
-abort;elsif (x+1)>l;raise "mvo: please specify different number";abort
-end;b = self[(x+1)];xb=self[x];self[(x+1)]=xb;self[x]=b;return self;end
-
