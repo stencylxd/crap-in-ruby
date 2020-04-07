fe02edf4726298c394414570eb07b6974a12e3af
Leo Markley <stencylxd@gmail.com>
2020-04-07 17:39:53 -0400

    Final release of Pong!

--- pong_for_my_dad/pong.rb
+++ pong_for_my_dad/pong.rb
@@ -2,15 +2,15 @@
 # pong, but for my dad | stencylxd 2020
 
 require "ruby2d"; set width: 1000, height: 600, title: "Pong!"
-$song = Music.new('my plea.wav'); $song.loop = true; $song.play
-$ballDirection = :none; $start = false; $rmed = 3;
-$scr1 = 0; $scr2 = 0; $startB = false
-$diff = :impossible; $paused = true
+$song = Music.new('my plea.wav'); $song.loop = true; $song.play; $song.volume = 20
+$ballDirection = :none; $start = false; $rmed = 3; $rmedB = 3
+$scr1 = 0; $scr2 = 0; $startB = false; $mstopped = false; $bspeed = 2
+$diff = :impossible; $paused = true; $rmedBb = 3; $frames = 0
 $s1 = Text.new($scr1.to_s, color: "red", size: 40, x: 8, y: 5); $s1.remove
 $right = Rectangle.new(color: "blue", x: 940, y: 250, height: 100, width: 10); $right.remove
 $p1 = Rectangle.new(color: "red", x: 60, y: 250, height: 100, width: 10); $p1.remove
 def startGame(tmp = false, oth = false); clear; $b = Square.new(color: "white", size: 25, x: 475, y: 277)
-  if tmp; $p1.y = 250; $right.y = 250; $scr1 = 0; $scr2 = 0; end
+  if tmp; $p1.y = 250; $right.y = 250; $scr1 = 0; $scr2 = 0; $frames = 0; end
   if oth; $rmed = 3; $rmedB = 3; $p1.y = 250; $right.y = 250; end
   $s1 = Text.new($scr1.to_s, color: "red", size: 40, x: 8, y: 5);
   $p1.y = 250 if !$startB; if $diff != :impossible; $s1.add; else; $s1.remove; end
@@ -20,10 +20,15 @@ def startGame(tmp = false, oth = false); clear; $b = Square.new(color: "white", 
       $ballDirection = :leftup; when 2
       $ballDirection = :rightdown; when 3
       $ballDirection = :rightup; end; $paused = false; end
-on :key_held do |e|
-  $p1.y -= 3 if (e.key == "up") && $p1.y > 0 && $startB && $paused != true
-  $p1.y += 3 if (e.key == "down") && $p1.y < 500 && $startB && $paused != true
- $start = true if e.key == "return" && $startB == false; case e.key; when "1"
+on :key_down do |e|; abort if e.key == "escape"
+if e.key == "m"; case $mstopped; when true; $mstopped = 3
+$song.pause; when false; $mstopped = true; $song.resume; end; end
+$mstopped = false if $mstopped == 3; end; on :key_held do |e|
+  $p1.y -= 3 if (e.key == "up") && $p1.y > 0 && $startB && !$paused
+  $p1.y += 3 if (e.key == "down") && $p1.y < 500 && $startB && !$paused
+ if e.key == "return" && !$startB; $start = true; $frames = 0; $p1.y = 250
+  $right.y = 250; $scr1 = 0; $scr2 = 0; $frames = 0; $rmed = 3; $rmedB = 3
+  $p1.y = 250; $right.y = 250; end; case e.key; when "1"
  $diff = :easy; startGame(true); when "2"; $diff = :medium; startGame(true); when "3"
  $diff = :impossible; startGame(true); end; end
 def wallHit(n = false, sym = $ballDirection); if n == true
@@ -39,9 +44,10 @@ end; Image.new("cover.png", width: 436, height: 196, x: 275, y: 100)
 ball2 = Square.new(color: "white", size: 25, y: 65, x: 686)
 ball = Square.new(color: "white", size: 25, y: 310, x: 275); hitwall = false; hitwall2 = true
 Text.new("Copyright CC-BY-NC-SA | stencylxd", y: 570, x: 10, size: 15)
-Text.new("Press 1 for Easy Mode", y: 420, x: 360, size: 25);
-Text.new("Press 2 for Normal Mode", y: 440, x: 360, size: 25);
-Text.new("Press 3 for Impossible Mode", y: 460, x: 360, size: 25); update do; if $startB == false
+Text.new("Press 1 for Easy Mode", y: 420, x: 360, size: 25)
+Text.new("Press 2 for Normal Mode", y: 440, x: 360, size: 25)
+Text.new("Press 3 for Impossible Mode", y: 460, x: 360, size: 25)
+Text.new("Press M to toggle music.", y: 480, x: 360, size: 25); update do; if $startB == false
     if ball.x < 686 && !hitwall; ball.x += 3; elsif ball.x >= 686 || hitwall;
     hitwall = true; ball.x -= 3; hitwall = false if ball.x < 275; end
     if ball2.x < 686 && !hitwall2; ball2.x += 3; elsif ball2.x >= 686 || hitwall2;
@@ -51,25 +57,31 @@ Text.new("Press 3 for Impossible Mode", y: 460, x: 360, size: 25); update do; if
     $cputop = ($right.y - ($right.height / 2)) + 50
     $cpulow = ($right.y + ($right.height / 2)) + 50
     unless $paused == true; case $ballDirection; when :leftdown
-      $b.x -= 3; $b.y += 3; when :leftup; $b.x -= 3
-      $b.y -= 3; when :rightdown; $b.x += 3; $b.y += 3
-    when :rightup; $b.x += 3; $b.y -= 3; end; end
-    if $b.y > 575; $b.y -= 3; $ballDirection = wallHit;
-    elsif $b.y < 0; $b.y += 3; $ballDirection = wallHit; end
+      $b.x -= $bspeed; $b.y += $bspeed; when :leftup; $b.x -= $bspeed
+      $b.y -= $bspeed; when :rightdown; $b.x += $bspeed; $b.y += $bspeed
+    when :rightup; $b.x += $bspeed; $b.y -= $bspeed; end; end
+    if $b.y > 575; $b.y -= $bspeed; $ballDirection = wallHit;
+    elsif $b.y < 0; $b.y += $bspeed; $ballDirection = wallHit; end
     counter = $p1top; until counter == $p1low
-    if $b.contains?(60, counter); $b.x += 3; $ballDirection = wallHit(true)
+    if $b.contains?(60, counter); $b.x += $bspeed; $ballDirection = wallHit(true)
     end; counter += 1; end; counter = $cputop; until counter == $cpulow
-    if $b.contains?(940, counter); $b.x -= 3; $ballDirection = wallHit(true);$rmed=3;$rmedB=3;
+    if $b.contains?(940, counter); $b.x -= $bspeed; $ballDirection = wallHit(true);$rmed=3;$rmedB=3;
     end; counter += 1; end; if $b.x < 0; $scr2 += 1; startGame(false, true)
   elsif $b.x > 1000; $scr1 += 1; startGame(false, true); end
     $right.y = ($b.y - 25) if $diff == (:impossible || :ir) && ($diff != :medium)
-    if $diff == :medium; $rmed = rand(0..1) if $rmed == 3; if $rmed == 1
+    if $diff == :medium; $rmed = rand(0..2) if $rmed == 3; if $rmed == 1 || 2
         unless $b.y == $right.y; $right.y += 3 if $b.y > $right.y
         $right.y -= 3 if $b.y < $right.y; end
       elsif $rmed == 0; $rmedB = rand(0..1) if $rmedB == 3
       case $rmedB; when 0; if $right.y < 505; $right.y += 3
       else; $rmedB = 1; end; when 1; if $right.y > 0; $right.y -= 3
-    else; $rmedB = 0; end; end; end; end
+    else; $rmedB = 0; end; end; end; end; if $diff == :easy
+    $rmedBb = rand(0..1) if $rmedBb == 3
+    case $rmedBb; when 0; if $right.y < 505; $right.y += 3
+    else; $rmedBb = 1; end; when 1; if $right.y > 0; $right.y -= 3
+  else; $rmedBb = 0; end; end; end
+if $startB && !$paused; $divf = $frames / 240; $bspeed = 2
+  for nn in 1..$divf do; $bspeed += 0.12; end; end
     if $scr1 == 9; $paused = true; $startB = false; clear
       Text.new("You win! (enter to retry)", y: 480, x: 410)
       Image.new("cover.png", width: 436, height: 196, x: 275, y: 100)
@@ -78,4 +90,4 @@ Text.new("Press 3 for Impossible Mode", y: 460, x: 360, size: 25); update do; if
       Text.new("CPU wins! (enter to retry)", y: 480, x: 410)
       Image.new("cover.png", width: 436, height: 196, x: 275, y: 100)
       $scr1 = 0; $scr2 = 0; end; $right.y = 505 if $right.y > 505
-    $right.y = 0 if $right.y < 0; end; $song.loop; end; show
+    $right.y = 0 if $right.y < 0; end; $song.loop = true; $frames += 1; end; show
